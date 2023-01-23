6926392f75cd2bd6a0af273f8b1485774cf64648
huezxiii <huesli.olibyer.0134@gmail.com>
2023-01-23 10:52:49 +0800

    Initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = "Wesley Olivier rsdg";
+    $age = "32";
+
+    echo "Name is $name. $age years old."; ?>
+    <br>
+</body>
+
+</html>
+
