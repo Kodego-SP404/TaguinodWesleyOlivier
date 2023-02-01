01409e405983287a4ee80edfc3a897bd58c7b978
ronsau27 <ronsau27@gmail.com>
2023-02-01 15:39:30 +0800

    RonSau

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,6 +8,8 @@
    @vite('resources/css/app.css') 
    <script src="//unpkg.com/alpinejs" defer></script>
    <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+   
+
 </head>
 <body class="bg-gray-600 min-h-screen pt-12 pb-6 px-2" style="background-image: url({{ asset('assets/school.jpg')}}); background-size: cover; background-position: center; opacity: 1;">
 
