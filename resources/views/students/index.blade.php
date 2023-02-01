4ec79a5624453dbefda25b0a6dba8a7dbc4e417c
ronsau27 <ronsau27@gmail.com>
2023-02-01 13:23:31 +0800

    RonSau

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -5,12 +5,12 @@
 
 <header class="max-w-lg mx-auto mt-5">
    <a href="#">
-      <h1 class="text-4xl font-bold text-white text-center">Student List - Testing</h1>
+      <h1 class="text-4xl font-bold text-white text-center">Student List</h1>
    </a>
 </header>
 
  
-<section class="mt-10">
+<section>
    <div class=" overflow-x-auto relative">
       <div class="flex justify-center my-5 py-5">
          <form action="{{ route('student-list') }}" method="GET">
@@ -18,10 +18,10 @@
          <input type="text" name="last_name" placeholder="Last Name">
          <input type="text" name="email" placeholder="Email">
          <input type="text" name="age" placeholder="Age">
-         <button type="submit" class="">Filter</button>
+         <button type="submit" class="inline-block px-6 py-2 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Filter</button>
       </form></div>
       
-      <table class="w-96 mx-auto text-sm text-left text-gray-500" id="StudentList">
+      <table class="py-5 w-96 mx-auto text-sm text-left text-gray-500" id="StudentList">
          <thead class="text-xs text gray-700 uppercase bg-gray-50">
             <tr>
                <th scope="col" class="py-3 px-6" onclick="sortTable(0)">
